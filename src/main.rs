eb726da7fcb644eb5010f62db81dac5e8db6da6b
Zak Farmer <zak@3sidedcube.com>
2023-05-27 19:38:19 +0100

    Implemented one-dimensional automaton (Chapter 2)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use ggez::event::{self, EventHandler};
 use ggez::graphics::{self, BlendMode, Color};
 use ggez::{Context, ContextBuilder, GameResult};
-use rand::Rng;
 
 const CELL_SIZE: i32 = 10;
 
-const WIDTH: i32 = 1200;
-const HEIGHT: i32 = 1200;
+const WIDTH: i32 = 1920;
+const HEIGHT: i32 = 1080;
 
 struct MainState {
     grid: Vec<Vec<bool>>,
@@ -14,6 +13,8 @@ struct MainState {
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
+        ctx.gfx.set_window_title("Cellular Automata");
+        
         ctx.gfx.set_drawable_size(WIDTH as f32, HEIGHT as f32)?;
 
         let drawable_width = ctx.gfx.drawable_size().0;
@@ -22,11 +23,11 @@ impl MainState {
         let num_cols = (drawable_width / CELL_SIZE as f32) as usize;
         let num_rows = (drawable_height / CELL_SIZE as f32) as usize;
 
-        let rng = &mut rand::thread_rng();
+        let mut grid: Vec<Vec<bool>> = vec![vec![false; num_cols]; num_rows];
 
-        let grid: Vec<Vec<bool>> = (0..num_cols)
-            .map(|_| (0..num_rows).map(|_| rng.gen_bool(1.0 / 3.0)).collect())
-            .collect();
+        let grid_length = grid.len();
+        
+        grid[grid_length - 1][num_cols / 2] = true;
 
         let s = MainState { grid };
 
@@ -36,6 +37,24 @@ impl MainState {
 
 impl EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        // Make a copy of the last row
+        let old_row = self.grid[self.grid.len() - 1].clone();
+        
+        // Calculate the new row
+        let mut new_row = vec![false; old_row.len()];
+        for j in 0..old_row.len() {
+            let left = old_row[(j + old_row.len() - 1) % old_row.len()];
+            let center = old_row[j];
+            let right = old_row[(j + 1) % old_row.len()];
+
+            // Rule 30
+            new_row[j] = left ^ (center || right);
+        }
+
+        // Remove the first row and add the new row at the end
+        self.grid.remove(0);
+        self.grid.push(new_row);
+
         Ok(())
     }
 
